db0ca97daed80921ffb7d34658c32aa7119e780a
Andy <Andy@qq.com>
2021-11-17 16:08:06 +0800

    push commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,7 +9,7 @@ public class GitTest {
         System.out.println("master - test!!");
         System.out.println("hot-fix test!!");
         System.out.println("push test!!");
-        private Integer id;
+        Integer id;
  
         
     }
